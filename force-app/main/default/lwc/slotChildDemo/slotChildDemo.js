0ed2ced3cb6efa6a7c84e19af31d7769d5981e77
puneetgaur1977 <gaur.puneet@outlook.com>
2021-06-07 20:10:49 -0700

    Communication Commit

--- force-app/main/default/lwc/slotChildDemo/slotChildDemo.js
+++ force-app/main/default/lwc/slotChildDemo/slotChildDemo.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
 
-export default class SlotChildDemo extends LightningElement {}
+export default class SlotChildDemo extends LightningElement {
+    handleSlotChange(event){
+        const footerElement = this.template.querySelector('.slds-card__footer');
+        /*this.template.querySelector('.main').textContent
+        this.template.querySelector('.conName:first-child')*/
+        if(footerElement){
+            footerElement.classList.remove('slds-hide');
+        }
+    }
+}
